89cdebe698b7094f710cafc50f0834b87a8e8e75
魏立雄 <weilixiong@users.noreply.github.com>
2026-05-14 11:36:03 +0800

    Fix offline submit approver next step

--- src/libs/actions/IOU/ReportWorkflow.ts
+++ src/libs/actions/IOU/ReportWorkflow.ts
@@ -1285,6 +1285,7 @@ function submitReport({
               hasViolations,
               isASAPSubmitBetaEnabled,
               isUnapprove: true,
+              bypassNextApproverID: managerID,
           });
     const optimisticNextStep = isDEWPolicy
         ? null
@@ -1297,6 +1298,7 @@ function submitReport({
               hasViolations,
               isASAPSubmitBetaEnabled,
               isUnapprove: true,
+              bypassNextApproverID: managerID,
           });
     const optimisticData: Array<
         OnyxUpdate<typeof ONYXKEYS.COLLECTION.REPORT_ACTIONS | typeof ONYXKEYS.COLLECTION.REPORT | typeof ONYXKEYS.COLLECTION.NEXT_STEP | typeof ONYXKEYS.COLLECTION.REPORT_METADATA>
